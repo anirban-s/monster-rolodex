6e7e1e60f0ab231ce2517285791321050e3d0a38
anirban-s <baban.chrome@gmail.com>
2019-07-13 03:16:33 +0530

    Updates

---
+++ precache-manifest.ee4253e76f8bf682806abbbfadbcd86c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e63ce6a9d61329fb90b4c7c26c398d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2855dcdadd1bc51c57d9",
+    "url": "/monster-rolodex/static/css/main.c7c22f9a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2855dcdadd1bc51c57d9",
+    "url": "/monster-rolodex/static/js/main.e9e5c79a.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
